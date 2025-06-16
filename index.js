1cab61c5c10124125b15ad75772bd21348e5b000
Seb005 <sbelisle@cjeao.qc.ca>
2025-06-16 19:49:00 -0400

    Supprime la dépendance à open

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const { google } = require('googleapis');
-const open = require('open');
 
 const app = express();
 const port = process.env.PORT || 3000;
